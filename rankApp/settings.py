c1fc923aa550ef1ad5ca1d52500d1d53891704b0
NPilis <norbertpilis@gmail.com>
2020-11-15 23:40:00 +0100

    Creating dummy data and some adjustmenst in frontend

--- rankApp/settings.py
+++ rankApp/settings.py
@@ -39,12 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken',
-    'rest_auth',
     'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'rest_auth.registration',
     'knox',
 
     'user.apps.UserConfig',
--- rankApp/urls.py
+++ rankApp/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('user.urls', namespace='user')),
     path('api/rankings/', include('ranking.urls', namespace='rankings')),
-    path('api/auth/', include('authentication.urls', namespace='authentication')),
-    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/auth/', include('authentication.urls', namespace='authentication'))
 ]
 
 if settings.DEBUG:
